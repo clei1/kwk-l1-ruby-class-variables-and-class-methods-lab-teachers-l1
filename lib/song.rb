02b9b46654dbf3ee5339b3ea64eb547b4ab8d103
clei1 <callmecon328@gmail.com>
2018-07-21 21:33:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  
+  @@count = 0
 
   def initialize(n, a, g)
     @name = n
     @artist = a
     @genre = g
+
+    count += 1
+  end
+
+  def count
+    return count
   end
 end
